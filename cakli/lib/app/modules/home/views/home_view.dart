e652f7669091939855f2271d9a487b7600baf2dc
MIKO-MILO <zaki.nh357305@gmail.com>
2026-02-19 08:57:30 +0700

    mencoba sabar home

--- cakli/lib/app/modules/home/views/home_view.dart
+++ cakli/lib/app/modules/home/views/home_view.dart
@@ -9,17 +9,9 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
       body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+        child: Image(image: AssetImage('assets/images/home/ilustrasi.svg'),),
       ),
-      
     );
   }
 }
